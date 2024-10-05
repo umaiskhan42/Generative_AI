06d3735e78016407f523bcfe35848c3e66404dca
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-10-05 19:56:47 +0500

    groq chatbot

--- chatbots/RAG_chatbot/app.py
+++ chatbots/RAG_chatbot/app.py
@@ -5,6 +5,17 @@ from langchain.chains import create_history_aware_retriever, create_retrievel_ch
 from langchain_chroma import Chroma 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
+## RAG Q&A Conversation With PDF Including Chat History
+import streamlit as st
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+import os
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -42,5 +53,25 @@ if api_key:
                 loader=PyPDFLoader(temppdf)
                 docs=loader.load()
                 documents.extend(docs)
-                
 
+        #split and create embeddings
+        text_splitter=RecursiveCharachterTextSplitter(chunk_size=5000,chunk_overlap=500)
+        splits=text_splitter.split_documents(documents)
+        vectorstore=Chroma.from_documents(documents=splits,embedding=embeddings)
+        retriever=vectorstore.as_retriever()
+
+        contextualize_q_system_prompt=(
+            "Given a chat history and the latest user question"
+            "which might reference context in the chat history, "
+            "formulate a standalone question which can be understood "
+            "without the chat history. Do NOT answer the question, "
+            "just reformulate it if needed and otherwise return it as is."
+
+        )
+        contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+        ]
+        )
