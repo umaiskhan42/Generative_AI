b326f789f662482b9c4528da8d71b0f1b53869da
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-10-05 20:01:22 +0500

    rag chatbot

--- chatbots/RAG_chatbot/app.py
+++ chatbots/RAG_chatbot/app.py
@@ -75,3 +75,30 @@ if api_key:
         ("human", "{input}"),
         ]
         )
+        history_aware_retriever=create_history_aware_retriever(llm,retriever,contextualize_q_prompt)
+
+
+        #Answer Question Prompt
+
+        system_prompt = (
+            "You are an assistant for question-answering tasks."
+            "Use the following pieces of retrieved context to answer"
+            "the question. If you don't know the answer, say that you"
+            "don't know. Use three sentences maximum and keep the "
+            "answer concise."
+            "\n\n"
+            "{context}"
+        )
+
+
+        qa_prompt = ChatPromptTemplate.from_messages(
+            [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+            ])
+        
+        question_answer_chain=create_stuff_documents_chain(llm,qa_prompt)
+        rag_chain=create_retrieval_chain(history_aware_retriever,question_answer_chain)
+
+        
